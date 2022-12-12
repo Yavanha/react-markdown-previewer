ceecfdbaf7a500a2cb2252c639133292a1b3ab33
Assadiki Yassin <yassin-it@pm.me>
2022-12-12 18:10:44 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css"
+
+import React from "react";
+import { marked } from "marked";
+
+export default class App extends React.Component { 
+
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+    </div>)
+  }
+
+}
